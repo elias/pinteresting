7dabe5f07dad827c43b1416da20bf88dc9737263
Elias Bizannes <github@eliasbizannes.com>
2014-12-25 18:45:55 -0800

    added devise

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,4 +75,9 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+    # Required for Heroku
+    # note to set this to your actual host
+   config.action_mailer.default_url_options = { :host => 'http://omr-pinteresting-elias.herokuapp.com/' }
+
 end
